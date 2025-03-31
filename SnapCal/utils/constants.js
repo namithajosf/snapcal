3f89afc2d1ddd7d6634dff105c167ee0858b3688
namithajosf <namithajosf@gmail.com>
2025-03-31 21:43:35 +0530

    Updated utils/

---
+++ SnapCal/utils/constants.js
@@ -0,0 +1,10 @@
+// Constants for the app
+export const MEALS = [
+    { type: 'Breakfast', recommended: 336, icon: require('../assets/breakfast.png') },
+    { type: 'Lunch', recommended: 403, icon: require('../assets/lunch.png') },
+    { type: 'Dinner', recommended: 403, icon: require('../assets/dinner.png') },
+    { type: 'Snacks', recommended: 201, icon: require('../assets/snack.png') },
+  ];
+  
+  export const WATER_GLASS_SIZE = 150; // ml
+  export const DAILY_WATER_GOAL = 8; // glasses

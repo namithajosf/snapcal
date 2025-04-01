a9dcca1891954d262952a65d07091f512cc6e8de
namithajosf <namithajosf@gmail.com>
2025-04-01 10:55:44 +0530

    Custom header

--- SnapCal/screens/HomeScreen.js
+++ SnapCal/screens/HomeScreen.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { StyleSheet, ScrollView, StatusBar } from 'react-native';
+import { StyleSheet, ScrollView, StatusBar, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../components/Header';
 import CalorieTracker from '../components/CalorieTracker';
 import MealsList from '../components/MealsList';
 import DrinksSection from '../components/DrinksSection';
+import { FontAwesome } from '@expo/vector-icons';
+
 
 export default function HomeScreen({ navigation }) {
+
   return (
     <>
       <StatusBar 
@@ -17,7 +20,14 @@ export default function HomeScreen({ navigation }) {
         style={styles.container} 
         edges={['top']}
       >
-        <Header />
+      <Header 
+        rightComponent={
+          <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+              <FontAwesome name="user-circle" size={24} color="#2F855A" />
+          </TouchableOpacity>
+        }
+      />
+
         <ScrollView style={styles.scrollView}>
           <CalorieTracker />
           <MealsList navigation={navigation} />
